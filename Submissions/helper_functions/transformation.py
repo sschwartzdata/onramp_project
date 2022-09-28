91b1c7a47f8607183caae82b43f1fc2ac7dd9be9
Sarah <94994663+sschwartzdata@users.noreply.github.com>
2022-09-28 13:06:30 -0700

    Update transformation.py
    added docstring

--- Submissions/helper_functions/transformation.py
+++ Submissions/helper_functions/transformation.py
@@ -1,11 +1,21 @@
 def data_type(df):
+    """
+    Updates the dataframe columns to the correct data type
+    Parameters: 
+    df: dataframe
+    Returns:
+    Transformed dataframe (data type)
+    """
     try:
+        # Only executes for artist table, else is passes to the next try
         df["followers"] = df["followers"].astype('int')
         df['popularity'] = df['popularity'].astype('int')
     except Exception:
         pass
 
     try:
+        # Change release_date to expected format
+        df['release_date'] = pd.to_datetime(df.release_date, format='%Y-%m-%d', errors='coerce')
         df['release_date'] = df['release_date'].astype('datetime64')
         df['total_tracks'] = df['total_tracks'].astype('int')
     except Exception:
